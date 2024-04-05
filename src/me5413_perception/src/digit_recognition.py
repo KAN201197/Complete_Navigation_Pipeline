950fbcb6b307827a41423454055d11f1571efc61
Saurabh Kamat <kamatsaurabh01@gmail.com>
2024-04-05 17:18:01 +0800

    Added image streaming

--- src/me5413_perception/src/digit_recognition.py
+++ src/me5413_perception/src/digit_recognition.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import rospy
-from sensor_msgs.msg import CompressedImage
+from sensor_msgs.msg import Image
 import cv2
 from cv_bridge import CvBridge, CvBridgeError
 
@@ -8,27 +8,18 @@ def callback(data):
     bridge = CvBridge()
     try:
         # Convert the ROS CompressedImage to OpenCV format
-        cv_image = bridge.compressed_imgmsg_to_cv2(data, "bgr8")
+        cv_image = bridge.imgmsg_to_cv2(data, desired_encoding='passthrough')
     except CvBridgeError as e:
         rospy.logerr("CvBridge Error: {0}".format(e))
-    
+
     # Display the image
     cv2.imshow("Image Window", cv_image)
     cv2.waitKey(3)
 
 def listener():
-    rospy.init_node('image_listener', anonymous=True)
-    rospy.Subscriber("/front/image_raw/compressedDepth", CompressedImage, callback)
-    
-    # Initialize window for display
-    cv2.namedWindow("Image Window", 1)
-    
-    # Spin until node is shutdown
-    try:
-        rospy.spin()
-    except KeyboardInterrupt:
-        print("Shutting down")
-    cv2.destroyAllWindows()
+    rospy.init_node('listener', anonymous=True)
+    rospy.Subscriber('/front/image_raw/', Image, callback)
+    rospy.spin()
 
 if __name__ == '__main__':
     listener()
