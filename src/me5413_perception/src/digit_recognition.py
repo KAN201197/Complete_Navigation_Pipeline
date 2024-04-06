d989afc99c8e18746d06b972fca6402c91423278
Yash Agarwal <yash.27.agarwal@gmail.com>
2024-04-06 14:08:22 +0800

    model working now

--- src/me5413_perception/src/digit_recognition.py
+++ src/me5413_perception/src/digit_recognition.py
@@ -5,45 +5,47 @@ import cv2
 from cv_bridge import CvBridge, CvBridgeError
 from ultralytics import YOLO
 import os
+import threading
 
+# Initialize global variables
 current_dir = os.path.dirname(os.path.abspath(__file__))
 weights_path = os.path.join(current_dir, '..', 'yolov8', 'best.pt')
 model = YOLO(weights_path)
+latest_image = None
+bridge = CvBridge()
+image_lock = threading.Lock()  # Ensure thread-safe access to the latest_image
 
-def callback(data):
-    bridge = CvBridge()
+def image_callback(data):
+    global latest_image
     try:
-        # Convert the ROS CompressedImage to OpenCV format
-        cv_image = bridge.imgmsg_to_cv2(data, desired_encoding='passthrough')
+        with image_lock:
+            # Convert ROS Image message to CV2 image
+            latest_image = bridge.imgmsg_to_cv2(data, desired_encoding='passthrough')
     except CvBridgeError as e:
         rospy.logerr("CvBridge Error: {0}".format(e))
 
-    # w, h = cv_image.shape[:2]               #512 640
-    # print(f'size {w}  {h}')
-    # Display the image
-    results = model(source = cv_image, imgsz = [512, 640])
-    for r in results:
-        r.boxes
-        r.show()
-    cv2.imshow("Image Window", cv_image)
-    cv2.waitKey(3)
+def timer_callback(event):
+    global latest_image
+    if latest_image is not None:
+        with image_lock:
+            # Copy the latest_image to prevent modification during processing
+            image_to_process = latest_image.copy()
+        
+        # Perform prediction
+        results = model.predict(source=image_to_process, imgsz=[512, 640], conf = 0.4)
 
+        results[0].probs.top5
+        results[0].boxes
 
-
-# def image_detection(image):
-#     result = model(image)
-#     boxes = result.boxes  # Boxes object for bounding box outputs
-#     masks = result.masks  # Masks object for segmentation masks outputs
-#     keypoints = result.keypoints  # Keypoints object for pose outputs
-#     probs = result.probs  # Probs object for classification outputs
-#     result.show() 
-    
-    pass
-
+        
+        # Visualization or further processing
+        cv2.imshow("Image Window", image_to_process)
+        cv2.waitKey(3)
 
 def listener():
     rospy.init_node('listener', anonymous=True)
-    rospy.Subscriber('/front/image_raw/', Image, callback)
+    rospy.Subscriber('/front/image_raw/', Image, image_callback)
+    timer = rospy.Timer(rospy.Duration(1), timer_callback)
     rospy.spin()
 
 if __name__ == '__main__':
