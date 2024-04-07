e5a69af7c7ccb232542c51379b1ab79c030e93df
Yash Agarwal <yash.27.agarwal@gmail.com>
2024-04-07 17:51:27 +0800

    code clean + shifted everything in a class

--- src/me5413_perception/src/digit_recognition.py
+++ src/me5413_perception/src/digit_recognition.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 import rospy
 from sensor_msgs.msg import Image
-from geometry_msgs import PointStamped
-from std_msgs import Header
+from geometry_msgs.msg import PointStamped
+from std_msgs.msg import Header
 import cv2
 from cv_bridge import CvBridge, CvBridgeError
 from ultralytics import YOLO
@@ -11,107 +11,91 @@ import threading
 import torch
 import numpy as np
 
+class DigitRecognizer:
+    TARGET = 3
 
+    def __init__(self):
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        weights_path = os.path.join(current_dir, '..', 'yolov8', 'best_detect.pt')
+        self.model = YOLO(weights_path)
+        self.bridge = CvBridge()
+        self.latest_image = None
+        self.depth_image = None
+        self.image_lock = threading.Lock()
+        self.depth_image_lock = threading.Lock()
+        self.pub_coordinate = None
 
-# Initialize global variables
-current_dir = os.path.dirname(os.path.abspath(__file__))
-weights_path = os.path.join(current_dir, '..', 'yolov8', 'best_detect.pt')
-model = YOLO(weights_path)
-latest_image = None
-depth_image = None
-bridge = CvBridge()
-image_lock = threading.Lock()
-depth_image_lock = threading.Lock()  # Ensure thread-safe access to the latest_image
-TARGET = 3
+    def image_callback(self, data):
+        try:
+            with self.image_lock:
+                self.latest_image = self.bridge.imgmsg_to_cv2(data, desired_encoding='passthrough')
+        except CvBridgeError as e:
+            rospy.logerr("CvBridge Error: {0}".format(e))
 
-def image_callback(data):
-    global latest_image
-    try:
-        with image_lock:
-            # Convert ROS Image message to CV2 image
-            latest_image = bridge.imgmsg_to_cv2(data, desired_encoding='passthrough')
-    except CvBridgeError as e:
-        rospy.logerr("CvBridge Error: {0}".format(e))
+    def depth_image_callback(self, data):
+        try:
+            with self.depth_image_lock:
+                self.depth_image = self.bridge.imgmsg_to_cv2(data, desired_encoding='passthrough')
+        except CvBridgeError as e:
+            rospy.logerr("CvBridge Error: {0}".format(e))
 
+    def timer_callback(self, event):
+        if self.latest_image is not None:
+            with self.image_lock:
+                image_to_process = self.latest_image.copy()
 
-def depth_image_callback(data):
-    global depth_image
-    try:
-        with depth_image_lock:
-            depth_image = bridge.imgmsg_to_cv2(data, desired_encoding='passthrough')
-        
-    except CvBridgeError as e:
-        rospy.logerr("CvBridge Error: {0}".format(e))
-    
+            results = self.model.predict(source=image_to_process, conf=0.5, save_conf=True, show=True)
 
-def timer_callback(event):
-    global latest_image
-    global depth_image
-    if latest_image is not None:
-        with image_lock:
-            # Copy the latest_image to prevent modification during processing
-            image_to_process = latest_image.copy()
-        
-        # Perform prediction
-        results = model.predict(source=image_to_process, conf = 0.5, 
-                                save_conf=True, show=True)
+            for r in results:
+                box = r.boxes
+                box.cpu()
+                box.numpy()
+                coordinates = box.xywh
+                classes = box.cls
+            np_coordinates = coordinates.numpy()
+            np_classes = classes.numpy()
 
-        for r in results:
-            box = r.boxes
-            box.cpu()
-            box.numpy() 
-            coordinates = box.xywh
-            classes = box.cls
-        np_coordinates = coordinates.numpy()
-        # print(f'All detected coordinates are:\n{np_coordinates}')
-        np_classes = classes.numpy()
-        # print(f'All detected classes are:\n{np_classes}')
+            index = self.find_number_in_array(np_classes, self.TARGET)
+            if index != -1:
+                [x1, y1, w, h] = np_coordinates[index, :]
+                x_center = int(x1 + w / 2)
+                y_center = int(y1 + h / 2)
+                if 0 <= x_center < self.depth_image.shape[0] and 0 <= y_center < self.depth_image.shape[1]:
+                    depth = self.depth_image[x_center, y_center]
+                    # print(f'Depth image coordinates: {depth}')
+                    self.coordinates_publisher(x_center, y_center, depth)
+                else:
+                    print("Out of bounds.")
 
-        index = find_number_in_array(np_classes, TARGET)
-        # print(f'Index of {TARGET}:\n{index}')
-        if index == -1:
-            print("Looking for target")
-        else:
-            [x1, y1, w, h] = np_coordinates[index, :]
-            print(x1, y1, w, h) 
-            x_center = int(x1+w/2)
-            y_center = int(y1+h/2)
-            if 0 <= x_center < depth_image.shape[0] and 0 <= y_center < depth_image.shape[1]:
-                depth = depth_image[x_center, y_center]
-                print(f'Depth image coordinates: {depth}')
-                coordinates_publisher(x_center, y_center, depth)
             else:
-                print("Index out of bounds.")
+                print("Looking for target.")
 
+    def coordinates_publisher(self, x, y, z):
+        msg = PointStamped()
+        header = Header()
+        header.stamp = rospy.Time.now()
+        header.frame_id = "Image frame"
+        msg.header = header
+        msg.point.x = x
+        msg.point.y = y
+        msg.point.z = z
+        self.pub_coordinate.publish(msg)
 
-def coordinates_publisher(x, y, z):
-    msg = PointStamped()
-    header = Header()
-    header.stamp = rospy.Time.now()
-    header.frame_id = "Image frame"
-    msg.header = header
-    msg.point.x = x
-    msg.point.y = y
-    msg.point.z = z
-    pub_coordinate.publish(msg)
-    
+    def find_number_in_array(self, array, number):
+        indices = np.where(array == number)[0]
+        if indices.size > 0:
+            return indices[0]
+        else:
+            return -1
 
-
-def find_number_in_array(array, number):
-    indices = np.where(array == number)[0]
-    
-    if indices.size > 0:
-        return indices[0]
-    else:
-        return -1
-
-def listener():
-    rospy.init_node('listener', anonymous=True)
-    rospy.Subscriber('/kinect/rgb/image_raw', Image, image_callback)
-    rospy.Subscriber('/kinect/depth/image_raw', Image, depth_image_callback)
-    pub_coordinate = rospy.Publisher('/target/coordinate', PointStamped, 10)
-    timer = rospy.Timer(rospy.Duration(1), timer_callback)
-    rospy.spin()
+    def run(self):
+        rospy.init_node('listener', anonymous=True)
+        rospy.Subscriber('/kinect/rgb/image_raw', Image, self.image_callback)
+        rospy.Subscriber('/kinect/depth/image_raw', Image, self.depth_image_callback)
+        self.pub_coordinate = rospy.Publisher('/target/coordinates', PointStamped, 10)
+        timer = rospy.Timer(rospy.Duration(1), self.timer_callback)
+        rospy.spin()
 
 if __name__ == '__main__':
-    listener()
+    recognizer = DigitRecognizer()
+    recognizer.run()
