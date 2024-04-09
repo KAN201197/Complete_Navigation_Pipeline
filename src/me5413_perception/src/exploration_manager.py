4d5ebc2da3c4d8f061d39851247a90c1df65ef4f
Saurabh Kamat <kamatsaurabh01@gmail.com>
2024-04-09 17:21:19 +0800

    Added comments and deleted unused files

--- src/me5413_perception/src/exploration_manager.py
+++ src/me5413_perception/src/exploration_manager.py
@@ -12,6 +12,10 @@ from me5413_perception.transformation import YoloObjectTransformationToMap
 from me5413_perception.feasible_path_finder import FeasiblePathFinder
 
 class ExplorationManager:
+    '''
+    ROS Node to explore a room, find a target and approach it. Target detection is
+    carried out using YOLOv8.
+    '''
     def __init__(self):
         rospy.init_node('exploration_manager')
         self.box_goal_name = '/box_3'
@@ -24,12 +28,13 @@ class ExplorationManager:
         self.yolo_transformation = YoloObjectTransformationToMap()
         self.feasible_path_finder = FeasiblePathFinder()
         self.timer = rospy.Timer(
-            rospy.Duration(1.0),
-            self.digit_recognizer.timer_callback)
+            rospy.Duration(1.0), self.digit_recognizer.timer_callback)
         self.client = actionlib.SimpleActionClient(
-            'move_base',
-            MoveBaseAction)
+            'move_base', MoveBaseAction)
 
+    '''
+    Blocking function to run exploration and detection actions
+    '''
     def run(self):
         self.__wait_for_message(self.box_goal_name)
         self.__move_base_client(
@@ -45,54 +50,91 @@ class ExplorationManager:
                 self.__move_base_client(
                     self.__pose_stamped_converter(feasible_goal_pose_stamped))
 
-        # if self.point_stamped is not None:
-        #     rospy.loginfo(f'Point: {self.point_stamped.point.x}, {self.point_stamped.point.y}, {self.point_stamped.point.z}')
-
+    '''
+    Callback function passed to YOLOv8 detector to output a
+    point in frame: /kinect_frame_optical
+    :param: point_stamped (type: geometry_msgs.msg.PointStamped)
+        Calculated point in camera frame
+    '''
     def point_stamped_callback(self, point_stamped: PointStamped):
-        rospy.loginfo(f'Point assigned: {point_stamped.point.x}, {point_stamped.point.y}, {point_stamped.point.z}')
+        rospy.loginfo(
+            f'Point in frame /kinect_frame_optical:'
+            f'{point_stamped.point.x},'
+            f'{point_stamped.point.y},'
+            f'{point_stamped.point.z}')
         self.point_stamped = point_stamped
 
     def travel_feedback_cb(self, data):
         pass
 
+    '''
+    Callback function to be passed on to move base client for goal
+    tracking
+    '''
     def recognition_feedback_cb(self, data):
         if self.point_stamped is not None:
             self.client.cancel_goal()
 
+    '''
+    Internal function to get goal information form parameter server
+    :param: param_name
+        Goal identifier
+    :return: goal
+        Goal message in the form of MoveBaseGoal
+    '''
     def __get_goal_coordinates(self, param_name):
         goal = MoveBaseGoal()
         goal.target_pose.header.frame_id = self.frame_id
         goal.target_pose.header.stamp = rospy.Time.now()
-        goal.target_pose.pose.position.x = rospy.get_param(param_name+'/position/x')
-        goal.target_pose.pose.position.y = rospy.get_param(param_name+'/position/y')
-        goal.target_pose.pose.orientation.z = rospy.get_param(param_name+'/orientation/z')
-        goal.target_pose.pose.orientation.w = rospy.get_param(param_name+'/orientation/w')
+        goal.target_pose.pose.position.x = rospy.get_param(
+            param_name+'/position/x')
+        goal.target_pose.pose.position.y = rospy.get_param(
+            param_name+'/position/y')
+        goal.target_pose.pose.orientation.z = rospy.get_param(
+            param_name+'/orientation/z')
+        goal.target_pose.pose.orientation.w = rospy.get_param(
+            param_name+'/orientation/w')
         return goal
 
+    '''
+    Internal function to connect to move_base action server and send goals
+    :param: move_base_goal (type: move_base_msgs.msg.MoveBaseGoal)
+        Goal to me sent to move_base server
+    :param(optional): feedback_cb
+        Callback function to receive goal feedback
+    '''
     def __move_base_client(self, move_base_goal, feedback_cb=None):
         self.client.wait_for_server()
         rospy.loginfo('Connected')
-        # goal = MoveBaseGoal()
-        # goal.target_pose.header.frame_id = self.frame_id
-        # goal.target_pose.header.stamp = rospy.Time.now()
-        # goal.target_pose.pose.position.x = rospy.get_param(param_name+'/position/x')
-        # goal.target_pose.pose.position.y = rospy.get_param(param_name+'/position/y')
-        # goal.target_pose.pose.orientation.z = rospy.get_param(param_name+'/orientation/z')
-        # goal.target_pose.pose.orientation.w = rospy.get_param(param_name+'/orientation/w')
 
         self.client.send_goal(move_base_goal, feedback_cb=feedback_cb)
         result = self.client.wait_for_result()
         if result:
             rospy.loginfo(self.client.get_goal_status_text())
 
+    '''
+    One time subscriber to listen on /rviz_panel/goal_name
+    :param: goal
+        Goal name string to check the current message
+    '''
     def __wait_for_message(self, goal):
         while True:
             rospy.loginfo('Waiting')
-            msg = rospy.wait_for_message('rviz_panel/goal_name', String)
+            msg = rospy.wait_for_message(
+                'rviz_panel/goal_name',
+                String)
             rospy.loginfo(f'Received message: {msg.data}')
             if msg.data == goal:
                 break
 
+    '''
+    Internal function to convert from geometry_msgs.msg.PoseStamped
+    to move_base_msgs.msg.MoveBaseGoal
+    :param: msg (type: geometry_msgs.msg.PoseStamped)
+        Message in the form of PoseStamped
+    :return: goal
+        Message in the form of MoveBaseGoal
+    '''
     def __pose_stamped_converter(self, msg: PoseStamped):
         goal = MoveBaseGoal()
         goal.target_pose.header.frame_id = self.frame_id
@@ -103,6 +145,13 @@ class ExplorationManager:
         goal.target_pose.pose.orientation.w = msg.pose.orientation.w
         return goal
 
+    '''
+    Debugging function to convert ROS message to JSON
+    for printing to console
+    :param: data
+        ROS message of any type
+    :return: Message in the form of string
+    '''
     def __msg_to_json(self, data):
         msg_yaml = yaml.load(str(data))
         return json.dumps(msg_yaml, indent=4)
--- src/me5413_perception/src/me5413_perception/digit_recognition.py
+++ src/me5413_perception/src/me5413_perception/digit_recognition.py
@@ -8,10 +8,17 @@ import os
 import threading
 import numpy as np
 
+# Absolute path of YOLOv8 weights
 current_dir = os.path.dirname(os.path.abspath(__file__))
 weights_path = os.path.join(current_dir, 'yolov8', 'best_detect.pt')
 
 class DigitRecognizer:
+    '''
+    Recognition of digits using YOLOv8
+    :param: callback_fn
+        Callback function for passing the point in the form
+        of geometry_msgs.msg.PointStamped
+    '''
     def __init__(self, callback_fn):
         self.target = 3
         self.bridge = CvBridge()
@@ -21,26 +28,47 @@ class DigitRecognizer:
         self.depth_image_lock = threading.Lock()
         self.callback_fn = callback_fn
         self.model = YOLO(weights_path)
-        self.sub_depth_camera = rospy.Subscriber('/kinect/rgb/image_raw', Image, self.image_callback)
-        self.sub_camera = rospy.Subscriber('/kinect/depth/image_raw', Image, self.depth_image_callback)
+        self.sub_depth_camera = rospy.Subscriber(
+            '/kinect/rgb/image_raw',
+            Image, self.image_callback)
+        self.sub_camera = rospy.Subscriber(
+            '/kinect/depth/image_raw',
+            Image, self.depth_image_callback)
 
     # def set_target(self, target):
     #     self.TARGET = target
 
+    '''
+    Callback function listening on camera topic /kinect/rgb/image_raw.
+    Internally converts from sensor_msgs.msg.Image to cv::Mat
+    :param: data (type: sensor_msgs.msg.Image)
+        Image from topic
+    '''
     def image_callback(self, data):
         try:
             with self.image_lock:
-                self.latest_image = self.bridge.imgmsg_to_cv2(data, desired_encoding='passthrough')
+                self.latest_image = self.bridge.imgmsg_to_cv2(
+                    data, desired_encoding='passthrough')
         except CvBridgeError as e:
             rospy.logerr("CvBridge Error: {0}".format(e))
 
+    '''
+    Callback function listening on camera topic /kinect/depth/image_raw.
+    Internally converts from sensor_msgs.msg.Image to cv::Mat
+    :param: data (type: sensor_msgs.msg.Image)
+        Image from topic
+    '''
     def depth_image_callback(self, data):
         try:
             with self.depth_image_lock:
-                self.depth_image = self.bridge.imgmsg_to_cv2(data, desired_encoding='passthrough')
+                self.depth_image = self.bridge.imgmsg_to_cv2(
+                    data, desired_encoding='passthrough')
         except CvBridgeError as e:
             rospy.logerr("CvBridge Error: {0}".format(e))
 
+    '''
+    Timer callback running the YOLOv8 pipeline
+    '''
     def timer_callback(self, event):
         with self.image_lock:
             if self.latest_image is None:
@@ -88,6 +116,19 @@ class DigitRecognizer:
             return
         self.callback_fn(self.__create_coordinate_msg(x_center, y_center, depth))
 
+    '''
+    Internal function to create coordinate message of type
+    geometry_msgs.msg.PointStamped
+    :param: x
+        X Coordinate of center point of bounding box
+        in camera frame
+    :param: y
+        Y Coordinate of center point of bounding box
+        in camera frame
+    :param: z
+        Depth value extracted from depth image at (X, Y)
+    :return: Message of type geometry_msgs.msg.PointStamped
+    '''
     def __create_coordinate_msg(self, x, y, z):
         msg = PointStamped()
         header = Header()
@@ -99,6 +140,16 @@ class DigitRecognizer:
         msg.point.z = z
         return msg
 
+    '''
+    Intrernal function to find a number in a numpy array
+    and return index
+    :param: array
+        Numpy array of numbers
+    :param: number
+        Number to find
+    :returns: Index of the number (if multiple occurances then
+        the first index is returned) otherwise -1 if not found
+    '''
     def __find_number_in_array(self, array, number):
         indices = np.where(array == number)[0]
         print(indices)
